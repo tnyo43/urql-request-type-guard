4152d0677afd7a454324466a3585908e55b5a0cc
Tomoya Kashifuku <tnyo43@gmail.com>
2023-06-16 08:01:14 +0900

    implement a component according to the following tutorial ref: https://formidable.com/open-source/urql/docs/basics/react-preact/#run-a-first-query

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+import { useQuery } from 'urql';
 import { graphql } from './gql';
 
 const TodosQuery = graphql(`
@@ -8,3 +10,22 @@ const TodosQuery = graphql(`
     }
   }
 `);
+
+const Todos = () => {
+  const [result, reexecuteQuery] = useQuery({
+    query: TodosQuery
+  });
+
+  const { data, fetching, error } = result;
+
+  if (fetching) return <p>Loading...</p>;
+  if (error) return <p>Oh no... {error.message}</p>;
+
+  return (
+    <ul>
+      {data.todos.map((todo) => (
+        <li key={todo.id}>{todo.title}</li>
+      ))}
+    </ul>
+  );
+};
